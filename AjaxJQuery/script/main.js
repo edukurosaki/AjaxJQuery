60fd3fcd1ecee43b04d911dd968b6e3a247009fc
Eduardo Francisco <kurosaki.yslan@gmail.com>
2020-11-12 14:48:36 -0300

    Finalizado o curso co sucesso

--- AjaxJQuery/script/main.js
+++ AjaxJQuery/script/main.js
@@ -1,24 +1,32 @@
-
-
 function consultaCEP() {
+    $(".status").show();
     var cep = document.getElementById("cep").value;
     var url = "https://viacep.com.br/ws/" + cep + "/json/";
-    
-    
+
+
+
     $.ajax({
-       
+
         url: url,
         type: "GET",
-        success: function(response){
+        success: function (response) {
             console.log(response);
-            document.getElementById("cp").innerHTML = response.cep;
-            document.getElementById("uf").innerHTML = response.uf;
-            document.getElementById("logradouro").innerHTML = response.logradouro;
-            document.getElementById("bairro").innerHTML = response.bairro;
-            document.getElementById("localidade").innerHTML = response.localidade;
-
+            $("#result").html("Dados referente ao CEP: "+response.cep);
+            $("#titulo-cep").html(response.cep);
             $("#logradouro").html(response.logradouro);
-                    }
+            $("#bairro").html(response.bairro);
+            $("#cidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $("#complemento").html(response.complemento);
+            $(".cep").show();
+            $(".status").hide();
+
+
+        }
     })
 }
+$(function(){
+    $(".cep").hide();
+    $(".status").hide();
+})
 
